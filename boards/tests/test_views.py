fc5f9288ba14988d95f2036dcfa09a1a182d8547
Pacras <791684719@qq.com>
2019-03-27 23:27:27 +0800

    添加用户注册、登陆、注销功能，重新组织页面代码减少重复

--- boards/tests/test_views.py
+++ boards/tests/test_views.py
@@ -2,9 +2,9 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.contrib.auth.models import User
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
 # Create your tests here.
 
 
